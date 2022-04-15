3bdc555e4148ef532b342131613f0b2ab6ecf85a
Neelima Mohanty <97789856+neelimagoogly@users.noreply.github.com>
2022-04-15 19:32:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-(function () {
-  'use strict'
-
-  var forms = document.querySelectorAll('.needs-validation')
-
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
 function validate() {
   var textarea = $("#exampleFormControlTextarea1");
   var letters = /^[0-9a-zA-Z .!]+$/;
@@ -29,4 +12,53 @@ function validate() {
     textarea.addClass("is-valid");
     $("#alert").html("");
   }
+}
+function isbncheck(inputtxt)
+{
+  var isbn = /^\d{13}$/;
+  if(inputtxt.value.match(isbn))
+        {
+      alert("Yes,this is a valid ISBN")    
+      return true;
+        }
+      else
+        {
+        alert("Please enter a valid ISBN");
+        return false;
+        }
+}
+function required()
+{
+var empt = document.forms["form1"]["text1"].value;
+var letterNumber = /^[0-9a-zA-Z]+$/;
+if (empt == "")
+{
+alert("This is an obviously invalid quote");
+return true;
+}
+else if((inputtxt.value.match(letterNumber)))
+{
+alert("This is an obviously invalid quote");
+
+return true;
+}
+else 
+{
+alert("This is valid quote");
+return false; 
+}
+}
+function urlcheck(inputtxt)
+{
+  var url = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+  if(inputtxt.value.match(url))
+        {
+      alert("Yes,this is a valid URL")    
+      return true;
+        }
+      else
+        {
+        alert("Please enter a valid URL");
+        return false;
+        }
 }
